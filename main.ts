e4b44417e11a5609b1c8233a382a3decde713784
shivshorya <125136497+shivshorya@users.noreply.github.com>
2025-07-15 16:32:41 +0530

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  console.log('App running at http://localhost:3000');
+}
+bootstrap();
